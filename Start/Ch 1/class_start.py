d052132695b455e38b134294708703dbc7cd196f
patrickmlr <124625101+patrickmlr@users.noreply.github.com>
2025-12-05 13:29:44 +0000

    Finish chapter 1

--- Start/Ch 1/class_start.py
+++ Start/Ch 1/class_start.py
@@ -4,26 +4,45 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
 
     # TODO: double-underscore properties are hidden from other classes
+    __book_list = None
 
     # TODO: create a class method
+    @classmethod
+    def get_book_types(cls):
+        return cls.BOOK_TYPES
 
     # TODO: create a static method
+    @staticmethod
+    def get_book_list():
+        if Book.__book_list is None:
+            Book.__book_list = []
+        return Book.__book_list
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def set_title(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} is not a valid book type")
+        else:
+            self.booktype = booktype
 
 
 # TODO: access the class attribute
-
+print("Book types: ", Book.get_book_types())
 
 # TODO: Create some book instances
-
+b1 = Book("Title1", "HARDCOVER")
+b2 = Book("Title2", "PAPERBACK")
 
 # TODO: Use the static method to access a singleton object
+the_books = Book.get_book_list()
+the_books.append(b1)
+the_books.append(b2)
+print(the_books)
--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -5,21 +5,36 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price):
         self.title = title
         # TODO: add properties
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.__secret = "This is a secret attribute"
 
     # TODO: create instance methods
+    def get_price(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
+
+    def set_discount(self, amount):
+        self._discount = amount
 
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+b2 = Book("The Catcher in the Rye", "J.D. Salinger", 277, 29.95)
 
-# TODO: print the price of book1
+# # TODO: print the price of book1
+# print(b1.get_price())
 
-
-# TODO: try setting the discount
-
+# # TODO: try setting the discount
+# print(b2.get_price())
+# b2.set_discount(0.25)
+# print(b2.get_price())
 
 # TODO: properties with double underscores are hidden by the interpreter
+print(b2._Book__secret)
